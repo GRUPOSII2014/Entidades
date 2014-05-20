84f7233d8fd50cce3f866c5b08577682486565ad
Emilio Tenorio <emiliotenorio@gmail.com>
2014-05-20 18:00:59 +0200

    Cambios en JPA Noticia

--- Noticia.java
+++ Noticia.java
@@ -46,7 +46,7 @@ public class Noticia implements Serializable {
     
     @ManyToOne
     @JoinColumn(name = "escritoPor")
-    private TrabajadoresHospital escritoPor;
+    private Trabajador escritoPor;
 
     public Long getId() {
         return id;
@@ -80,11 +80,11 @@ public class Noticia implements Serializable {
         this.descripcion = descripcion;
     }
 
-    public TrabajadoresHospital getEscritoPor() {
+    public Trabajador getEscritoPor() {
         return escritoPor;
     }
 
-    public void setEscritoPor(TrabajadoresHospital escritoPor) {
+    public void setEscritoPor(Trabajador escritoPor) {
         this.escritoPor = escritoPor;
     }
 
